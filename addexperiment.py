10b1e605c793a82430f3500dbf5a7057dbb7dd4a
Jyotsnarajan <jyotsnarajan.815@gmail.com>
2020-03-30 16:19:45 +0530

    improved script again

--- addexperiment.py
+++ addexperiment.py
@@ -3,6 +3,7 @@ from urllib.request import urlopen
 import requests
 import json
 import argparse
+from PIL import Image
 
 url = 'http://localhost/api/archive/add_experiment'
 
@@ -11,6 +12,16 @@ ap = argparse.ArgumentParser()
 ap.add_argument("--image", "-i", required = True, help = "Path to input image")
 ap.add_argument("--image1", "-i1", required = True, help = "Path to input image")
 args = vars(ap.parse_args())
+  
+# creating thumbnails  
+image = Image.open(args['image'])
+image1 = Image.open(args['image1'])
+MAX_SIZE = (100, 100)
+MAX_SIZE1 = (100, 100)
+image.thumbnail(MAX_SIZE)
+image1.thumbnail(MAX_SIZE) 
+image.save('catthumb.png')
+image.save('votesthumb.png')
 
 #add an experiment to the archive
 def add_experiment(demo_id, blobs, parameters):
@@ -33,7 +44,7 @@ def add_experiment(demo_id, blobs, parameters):
 		print(response)
 		
 #calling the experiment
-add_experiment(77777000049, [{"input image": args['image']}, {"votes": args['image1']}], {"trend": 100})
+add_experiment(77777000049, [{"input image": args['image'], "thumb": "catthumb.png"}, {"votes": args['image1'], "thumb1": "votesthumb.png"}], {"trend": 100})
     
     
     
