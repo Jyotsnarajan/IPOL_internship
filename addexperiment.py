46489de3304bd170beb023c068618c55d928d881
Jyotsnarajan <jyotsnarajan.815@gmail.com>
2020-04-02 15:26:17 +0530

    modified with thumbnails

--- addexperiment.py
+++ addexperiment.py
@@ -9,19 +9,20 @@ url = 'http://localhost/api/archive/add_experiment'
 
 #parse the arguments
 ap = argparse.ArgumentParser()
-ap.add_argument("--image", "-i", required = True, help = "Path to input image")
-ap.add_argument("--image1", "-i1", required = True, help = "Path to input image")
+ap.add_argument("--cat", "-i", required = True, help = "Path to input image")
+ap.add_argument("--votes", "-i1", required = True, help = "Path to input image")
+ap.add_argument("--catthumb", "-it", required = True, help = "Path to input image")
+ap.add_argument("--votesthumb", "-it1", required = True, help = "Path to input image")
 args = vars(ap.parse_args())
-  
+
 # creating thumbnails  
-image = Image.open(args['image'])
-image1 = Image.open(args['image1'])
+image = Image.open(args['cat'])
+image1 = Image.open(args['votes'])
 MAX_SIZE = (100, 100)
-MAX_SIZE1 = (100, 100)
 image.thumbnail(MAX_SIZE)
 image1.thumbnail(MAX_SIZE) 
-image.save('catthumb.png')
-image.save('votesthumb.png')
+image.save(args['catthumb'])
+image1.save(args['votesthumb'])
 
 #add an experiment to the archive
 def add_experiment(demo_id, blobs, parameters):
@@ -44,7 +45,7 @@ def add_experiment(demo_id, blobs, parameters):
 		print(response)
 		
 #calling the experiment
-add_experiment(77777000049, [{"input image": args['image'], "thumb": "catthumb.png"}, {"votes": args['image1'], "thumb1": "votesthumb.png"}], {"trend": 100})
+add_experiment(77777000049, [{"input image": args['cat'], "thumbnail": args['catthumb']}, {"votes": args['votes'], "thumbnail": args['votesthumb']}], {"trend": 100})
     
     
     
