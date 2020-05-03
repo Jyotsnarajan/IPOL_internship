b16f6dd342ebf91f24083300fc864e9ba5414342
Jyotsnarajan <jyotsnarajan.815@gmail.com>
2020-05-04 03:57:11 +0530

    read index.cfg & extract params from archive of a demo

--- read_archive.py
+++ read_archive.py
@@ -20,8 +20,24 @@ def read_archive(directory):
 			config.read(exp_path)
 			config_dict = {s: dict(config.items(s))for s in config.sections()}
 			
-			print(config_dict['info'])
-			print(config_dict['fileinfo'])		    		  		    
+			print(f'\nThe experiment was performed on: "{config_dict["meta"]["date"]}"')
+			
+			number_of_params = len(config_dict['info'].items())
+			print(f'There are {number_of_params} parameters')
+			for key, value in config_dict['info'].items():
+				print(f' "{key}": "{value}"')
+				
+			files = config_dict["meta"]['files']			
+			files_list = files.split()
+			
+			number_of_blobs = len(config_dict['fileinfo'].items())
+			print(f'There are {number_of_blobs} blobs')
+			
+			for file in files_list:
+				if file in config_dict['fileinfo']:
+					print(f' Name: "{config_dict["fileinfo"][file]}", File: "{file}"')					
+				else:
+					raise FileNotFoundError(file)																
 		else:
 			break														
 
