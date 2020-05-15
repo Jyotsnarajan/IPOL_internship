f6ee47b1e4036cb9151cf486962832f63d49e1ef
Jyotsnarajan <jyotsnarajan.815@gmail.com>
2020-05-15 14:54:25 +0530

    read index.cfg and extract params

--- read_archive.py
+++ read_archive.py
@@ -17,35 +17,33 @@ def read_archive(directory):
 
     for idx, exp_path in enumerate(archive):
 
-        if idx < 10:
-            config = ConfigParser()
-            config.read(exp_path)
-            config_dict = {s: dict(config.items(s))for s in config.sections()}
-
-            print(f'\nThe experiment was performed on: "{config_dict["meta"]["date"]}"')
-
-            number_of_params = len(config_dict['info'].items())
-            print(f'There are {number_of_params} parameters')
-            for key, value in config_dict['info'].items():
-                print(f' "{key}": "{value}"')
-
-            files = config_dict["meta"]['files']
-            files_list = files.split()
-
-            number_of_blobs = len(config_dict['fileinfo'].items())
-            print(f'There are {number_of_blobs} blobs')
-
-            dir_path = os.path.dirname(exp_path)
-
-            for file in files_list:
-                file_path = os.path.join(dir_path, file)
-                if os.path.isfile(file_path):
-                    print(f' Name: "{config_dict["fileinfo"][file]}", File: "{file}"')
-                else:
-                    raise FileNotFoundError(file)
-
-        else:
+        if idx >= 10:
             break
+        
+        config = ConfigParser()
+        config.read(exp_path)
+        config_dict = {s: dict(config.items(s))for s in config.sections()}
+
+        print(f'\nThe experiment was performed on: "{config_dict["meta"]["date"]}"')
+
+        number_of_params = len(config_dict['info'].items())
+        print(f'There are {number_of_params} parameters')
+        for key, value in config_dict['info'].items():
+            print(f' "{key}": "{value}"')
+
+        files = config_dict["meta"]['files']
+        files_list = files.split()
+
+        number_of_blobs = len(config_dict['fileinfo'].items())
+        print(f'There are {number_of_blobs} blobs')
+
+        dir_path = os.path.dirname(exp_path)
+
+        for file in files_list:
+            file_path = os.path.join(dir_path, file)
+            if not os.path.isfile(file_path):
+                raise FileNotFoundError(file)
+            print(f' Name: "{config_dict["fileinfo"][file]}", File: "{file}"')
 
 # parse the arguments
 ap = argparse.ArgumentParser()
