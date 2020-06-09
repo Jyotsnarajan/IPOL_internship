40899ece9999c657ac565ec100808b658011d445
Jyotsnarajan <jyotsnarajan.815@gmail.com>
2020-06-10 02:18:46 +0530

    add blobs to the new system

--- port_blobs(integration).py
+++ port_blobs(integration).py
@@ -59,9 +59,13 @@ def gen_input(directory):
     conf.read(os.path.join(directory, 'index.cfg'))
     for name, blob in [(s, dict(conf.items(s)))for s in conf.sections()]:
         path = os.path.join(directory, blob.get('files', ''))
+        path = os.path.join(directory, 'index.cfg')
+        assert os.path.isfile(path)
+        conf.read(path)
         title = blob['title']
         credit = blob['credit']
         yield path, title, credit
+
                           
 # parse the arguments
 ap = argparse.ArgumentParser()
@@ -73,17 +77,32 @@ args = ap.parse_args()
 demo_id = args.demo_id
 blobs = args.blobs
 
-#adding all the blobs
-for count, (path, title, credit) in enumerate(gen_input(blobs), 1):
-    add_blob_to_demo(path, demo_id, title, credit)
-
 # deleting all the blobs
 demo_blobs = get_blobs(demo_id)
 keys = demo_blobs.keys()
 sets = demo_blobs['sets']
+
+#print(f"set={sets}")
+
+
 for set in sets:
     for key in set['blobs'].keys():
         blob_set = set['name']
         pos_set = key
         print(f'deleting the blob_set: {blob_set}')
         remove_blob_from_demo(demo_id, blob_set , pos_set)
+
+#adding all the blobs
+
+metadata = gen_input(blobs)
+print(f"metadata={metadata}")
+'''
+for elem in metadata:
+    print(f'elem={elem}')
+
+sys.exit(0)
+'''
+
+for count, (path, title, credit) in enumerate(metadata, 1):
+    print(count, path, title, credit)
+    add_blob_to_demo(path, demo_id, title, credit)
