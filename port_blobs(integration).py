4090bb40eb99e7d5fc53d9176e5ddd6b0a0b3d28
Jyotsnarajan <jyotsnarajan.815@gmail.com>
2020-06-11 17:35:40 +0530

    add blobs to the new system

--- port_blobs(integration).py
+++ port_blobs(integration).py
@@ -14,7 +14,6 @@ def get_blobs(demo_id):
     '''
     params = {'demo_id': demo_id}
     response = requests.get('http://integration.ipol.im/api/blobs/get_blobs', params=params).json()
-    #print(response)
     return response
 
 def remove_blob_from_demo(demo_id, blob_set, pos_set):
@@ -25,12 +24,9 @@ def remove_blob_from_demo(demo_id, blob_set, pos_set):
             'pos_set': pos_set,
             'blob_set': blob_set}
     response = requests.delete('http://integration.ipol.im/api/blobs/remove_blob_from_demo', params=params).json()
+    print(response)
 
-    try:
-        assert response['status'] == 'OK', "error deleting the blobs"
-        print(response)
-    except AssertionError as msg:
-        print(msg)
+    assert response['status'] == 'OK'
 
 def add_blob_to_demo(path, demo_id, title, credit):
     '''
@@ -43,12 +39,9 @@ def add_blob_to_demo(path, demo_id, title, credit):
     }
     files = {'blob': urlopen('file:///' + path)}
     response = requests.post('http://integration.ipol.im/api/blobs/add_blob_to_demo', params=params, files=files).json()
+    print(response)
 
-    try:
-        assert response['status'] == 'OK', "error adding the blobs"
-        print(response)
-    except AssertionError as msg:
-        print(msg)
+    assert response['status'] == 'OK'
 
 def gen_input(directory):
     """
@@ -58,9 +51,6 @@ def gen_input(directory):
     conf.read(os.path.join(directory, 'index.cfg'))
     for name, blob in [(s, dict(conf.items(s)))for s in conf.sections()]:
         path = os.path.join(directory, blob.get('files', ''))
-        path = os.path.join(directory, 'index.cfg')
-        assert os.path.isfile(path)
-        conf.read(path)
         title = blob['title']
         credit = blob['credit']
         yield path, title, credit
@@ -81,26 +71,19 @@ demo_blobs = get_blobs(demo_id)
 keys = demo_blobs.keys()
 sets = demo_blobs['sets']
 
-#print(f"set={sets}")
-
+print("Deleting Blobs")
 
 for set in sets:
     for key in set['blobs'].keys():
         blob_set = set['name']
         pos_set = key
-        print(f'deleting the blob_set: {blob_set}')
         remove_blob_from_demo(demo_id, blob_set , pos_set)
 
 #adding all the blobs
 
 metadata = gen_input(blobs)
-print(f"metadata={metadata}")
-'''
-for elem in metadata:
-    print(f'elem={elem}')
-
-sys.exit(0)
-'''
+print("\n")
+print(f"Adding blobs, metadata={metadata}\n")
 
 for count, (path, title, credit) in enumerate(metadata, 1):
     print(count, path, title, credit)
