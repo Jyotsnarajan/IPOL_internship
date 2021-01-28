c652ec39e8962b186634d851be03cd0efe4ac95b
Jyotsnarajan <jyotsnarajan.815@gmail.com>
2021-01-28 16:32:46 +0530

    checking os.system values

--- run_nlmp.py
+++ run_nlmp.py
@@ -12,21 +12,22 @@ args = ap.parse_args()
 
 
 #string to bool
-args.add_noise = args.add_noise.lower() == 'true'
+add_noise = args.add_noise.lower() == 'true'
 
-noise = 1 if args.add_noise else 0
+add_noise = 1 if args.add_noise else 0
 
 #noisy and denoised images	
-p1 = f'NLMeansP input_0.png {args.sigma} {noise} input_1.png output_1.png'
-val1 = os.system(p1)
+p1 = f'NLMeansP input_0.png {args.sigma} {add_noise} input_1.png output_1.png'
+if os.system(p1)!= 0:
+	exit(-1)
 
 #compute image differences
 p2 = f'img_diff_ipol input_0.png output_1.png {args.sigma} output_2.png'
-val2 = os.system(p2)
+if os.system(p2)!= 0:
+	exit(-1)
 
 #estimate MSE and PSNR
 p3 = 'img_mse_ipol input_0.png output_1.png'
-val3 = os.system(p3)
+if os.system(p3)!= 0:
+	exit(-1)
 
-if val1 & val2 & val3 != 0:
-	sys.exit("'noise.png' not found")
