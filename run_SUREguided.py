71182b0ed0df32ab1967a6d657b23d3ab82cca6e
Jyotsnarajan <jyotsnarajan.815@gmail.com>
2021-01-28 18:32:43 +0530

    checking os.system values

--- run_SUREguided.py
+++ run_SUREguided.py
@@ -14,19 +14,10 @@ args = ap.parse_args()
 #string to bool
 add_noise = args.add_noise.lower() == 'true'
 
-add_noise = 1 if add_noise else 0
-
 darg = '-D' + str(4*float(args.sigma))
 noisearg = 'gaussian' + ':' + str(args.sigma)
 
-if add_noise == 0:	
-	p1 = f'denoiseSPLE input_0.png {args.sigma} {args.iteration} denoised.png'
-	if os.system(p1)!= 0: exit(-1)
-
-	p2 = f'imdiff {darg} input_0.png denoised.png diff.png'
-	if os.system(p2)!= 0: exit(-1)
-
-else:
+if add_noise:	
 	p1 = f'imnoise {noisearg} input_0.png noisy.png'
 	if os.system(p1)!= 0: exit(-1)
 	
@@ -35,3 +26,10 @@ else:
 
 	p3 = f'imdiff -mrmse input_0.png denoised.png diff.png'
 	if os.system(p3)!= 0: exit(-1)
+
+else:
+	p1 = f'denoiseSPLE input_0.png {args.sigma} {args.iteration} denoised.png'
+	if os.system(p1)!= 0: exit(-1)
+
+	p2 = f'imdiff {darg} input_0.png denoised.png diff.png'
+	if os.system(p2)!= 0: exit(-1)
