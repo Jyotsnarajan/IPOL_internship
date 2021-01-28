5fd73aafd5baccee49eed372baa3ba3554b85b99
Jyotsnarajan <jyotsnarajan.815@gmail.com>
2021-01-28 17:35:57 +0530

    checking os.system values

--- run_Rudin.py
+++ run_Rudin.py
@@ -11,32 +11,32 @@ ap.add_argument("noisemodel", type=str)
 args = ap.parse_args()
 
 #string to bool
-args.add_noise = args.add_noise.lower() == 'true'
+add_noise = args.add_noise.lower() == 'true'
 
-noise = 1 if args.add_noise else 0
+add_noise = 1 if args.add_noise else 0
 
 noisearg = str.lower(args.noisemodel) + ':' + str(args.sigma)
 darg = '-D' + str(4*float(args.sigma))
 
-if noise == 0:		
+if add_noise == 0:		
 	p1 = f'tvdenoise -n {noisearg} input_0.png denoised.png'
-	os.system(p1)
+	if os.system(p1)!= 0: exit(-1)
 	
 	p2 = f'imdiff {darg} input_0.png denoised.png diff.png'
-	os.system(p2)
+	if os.system(p2)!= 0: exit(-1)
 
 else:
 	p1 = f'imnoise {noisearg} input_0.png noisy.png'
-	os.system(p1)
+	if os.system(p1)!= 0: exit(-1)
 	
 	p2 = f'tvdenoise -n {noisearg} noisy.png denoised.png'
-	os.system(p2)
+	if os.system(p2)!= 0: exit(-1)
 	
 	p3 = 'imdiff -mrmse input_0.png noisy.png'
-	os.system(p3)
+	if os.system(p3)!= 0: exit(-1)
 
 	p4 = 'imdiff -mrmse input_0.png denoised.png'
-	os.system(p4)
+	if os.system(p4)!= 0: exit(-1)
 
 	p5 = f'imdiff {darg} input_0.png denoised.png diff.png'
-	os.system(p5)
+	if os.system(p5)!= 0: exit(-1)
